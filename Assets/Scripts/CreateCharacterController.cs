d1db5fce64f900cbe79fb5bfe0309f766d08f92b
NightmareFox12 <carloshenriquez392004@gmail.com>
2025-08-05 22:36:10 -0400

    add arrows for change character and fix bugs.

--- Assets/Scripts/CreateCharacterController.cs
+++ Assets/Scripts/CreateCharacterController.cs
@@ -1,4 +1,4 @@
-using System.IO;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -6,7 +6,10 @@ public class CreateCharacterController : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     public Image characterImage;
-    public Button arrowButton;
+    public Button buttonLeft;
+    public Button buttonRight;
+
+    public TextMeshProUGUI characterName;
 
     // string path = Application.streamingAssetsPath + "/Data/characters.json";
 
@@ -28,24 +31,50 @@ public class CreateCharacterController : MonoBehaviour
     // Sprite sprite = Sprite.Create(tex, new Rect(0, 0, tex.width, tex.height), Vector2.one * 0.5f);
     // characterImage.sprite = sprite;
 
+
+    //private vars
     private CharacterDataList characterList;
+    private int nextCharacterID = 0;
+
     void Awake()
     {
-        int nextCharacterID = 0;
-
         TextAsset jsonFile = Resources.Load<TextAsset>("Data/characters");
         Debug.Log(jsonFile);
 
         characterList = JsonUtility.FromJson<CharacterDataList>(jsonFile.text);
 
-        Debug.Log($"Personajes cargados: {characterList.characters.Length}");
-        characterImage.sprite = Resources.Load<Sprite>(characterList.characters[0].pathSprite);
+        //Load First character
+        CharacterData firstCharacter = characterList.characters[0];
+        characterImage.sprite = Resources.Load<Sprite>(firstCharacter.pathSprite);
+        characterName.text = firstCharacter.name;
 
-        arrowButton.onClick.AddListener(() =>
+        buttonLeft.onClick.AddListener(() =>
         {
-            characterImage.sprite = Resources.Load<Sprite>(characterList.characters[nextCharacterID].pathSprite);
-            nextCharacterID = nextCharacterID < characterList.characters.Length - 1 ? nextCharacterID + 1 : nextCharacterID = 0;
+            ChangeCharacter(true);
         });
+
+        buttonRight.onClick.AddListener(() =>
+        {
+            ChangeCharacter(false);
+        });
+
+    }
+
+    void ChangeCharacter(bool leftPressed)
+    {
+        if (leftPressed)
+        {
+            nextCharacterID = nextCharacterID > 0 ? nextCharacterID - 1 : characterList.characters.Length - 1;
+        }
+        else
+        {
+            nextCharacterID = nextCharacterID < characterList.characters.Length - 1 ? nextCharacterID + 1 : 0;
+        }
+
+        CharacterData currentCharacter = characterList.characters[nextCharacterID];
+
+        characterImage.sprite = Resources.Load<Sprite>(currentCharacter.pathSprite);
+        characterName.text = currentCharacter.name;
     }
 
     void Start()
